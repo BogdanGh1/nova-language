f218c3daf39d008a885cd56f8773afc186e584b5
Ghinea_Bogdan <bogdan.ghinea@cloudflight.io>
2024-07-03 14:33:27 +0300

    ex_prezentare

--- Backend/resources/ex_prezentare.js
+++ Backend/resources/ex_prezentare.js
@@ -1,22 +1,23 @@
 function start() {
-    global let n = random(4,10);
-    if(n%2 == 0){
-        print(factorial(15));
-    }
-    else{
+    global let n = random(5, 15);
+    if (n < 10) {
+        print(factorial(n));
+    } else {
         printSum();
     }
 }
+
 function factorial(x) {
     if (x == 0) {
         return 1;
     }
     return factorial(x - 1) * x;
 }
-function suma(){
-    let s=0;
-    for(let i=1;i<=n;i=i+1){
-        s=s+i;
+
+function printSum() {
+    let s = 0;
+    for (let i = 1; i <= n; i = i + 1) {
+        s = s + i;
     }
     print(s);
 }
